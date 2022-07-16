d09b55f416f93190e862a8faf548ef2eaa5d7770
Apirak Panatkool <apirakb@gmail.com>
2022-07-16 20:52:02 +0700

    first commit

---
+++ src/updateText.ts
@@ -0,0 +1,46 @@
+import { setRelaunchButton } from "@create-figma-plugin/utilities";
+import i18next from "i18next";
+
+function updateAllTextProperty() {
+  const tEn = figma.currentPage.findAll((node) => /##t.en/.test(node.name));
+  const tEnJson = (<TextNode>tEn[0]).characters;
+  const tEnJsonObject = JSON.parse(`{ ${tEnJson} }`);
+
+  i18next.init({
+    lng: "en", // if you're using a language detector, do not define the lng option
+    debug: true,
+    resources: {
+      en: {
+        translation: tEnJsonObject,
+      },
+    },
+  });
+
+  const textNodes = figma.currentPage.findAll((node) =>
+    /#t.|_#t./.test(node.name)
+  );
+
+  textNodes.forEach((textNode) => {
+    console.log(textNode.name);
+  });
+
+  // await Promise.all(
+  // console.log("bank");
+  // );
+}
+
+export default function () {
+  setRelaunchButton(figma.currentPage, "figma-i18next", {
+    description: "🔍 Update text",
+  });
+
+  updateAllTextProperty();
+
+  // updateAllTextProperty().then(() => {
+  //   figma.closePlugin("Updated 🎉");
+  // });
+
+  figma.closePlugin("Updated 🎉");
+}
+
+export { updateAllTextProperty };
